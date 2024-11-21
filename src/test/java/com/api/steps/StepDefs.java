a54011e15da000b4f3e66cd649f0a67f9ccf5a4f
Natarajan Kanakasabapathy <Natarajan.Kanakasabapathy@rabobank.nl>
2024-11-21 16:23:11 +0100

    Stepdef initiation

--- src/test/java/com/api/steps/StepDefs.java
+++ src/test/java/com/api/steps/StepDefs.java
@@ -1,4 +1,11 @@
 package com.api.steps;
 
+import com.api.context.TestContext;
+
 public class StepDefs {
+    private final TestContext context;
+
+    public StepDefs(TestContext context) {
+        this.context = context;
+    }
 }
