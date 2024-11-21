a3525ebbaab8d4b048126b72e91e3d01e8864bfe
Natarajan Kanakasabapathy <natarajankanakasabapathy@Natarajans-MacBook-Pro.local>
2024-11-21 16:33:52 +0100

    Modified tags

--- src/test/java/com/api/runner/TestRunner.java
+++ src/test/java/com/api/runner/TestRunner.java
@@ -12,7 +12,7 @@ import org.testng.annotations.DataProvider;
                 "json:target/cucumber-reports/CucumberTestReport.json"
         },
         monochrome = true,
-        tags = "@Tests"
+        tags = "@Testing"
 )
 public class TestRunner extends AbstractTestNGCucumberTests  {
     @Override
